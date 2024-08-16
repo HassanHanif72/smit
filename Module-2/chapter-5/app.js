c78164e9c46bf0f5e8adb13926d25d8d908fec09
hassanhanif72 <hassanhanif72@gmail.com>
2024-08-17 03:28:24 +0500

    Upload Chapter 5

---
+++ Module-2/chapter-5/app.js
@@ -0,0 +1,67 @@
+// Question 1
+var num1 = 3;
+var num2 = 5;
+var total = num1 + num2;
+document.write("Add of 3 and 5 is: " + total + "<br>");
+
+// Question 2
+var num1 = 3;
+var num2 = 5;
+var total = num1 - num2;
+document.write("Subtract of 3 and 5 is: " + total + "<br>");
+
+var num1 = 3;
+var num2 = 5;
+var total = num1 * num2;
+document.write("Multiply of 3 and 5 is: " + total + "<br>");
+
+var num1 = 3;
+var num2 = 5;
+var total = num1 / num2;
+document.write("Divide of 3 and 5 is: " + total + "<br>");
+
+var num1 = 3;
+var num2 = 5;
+var total = num1 % num2;
+document.write("Modulus of 3 and 5 is: " + total + "<br>");
+document.write("---------------------------------------------------------------------------<br>");
+
+// Question 3
+var value
+document.write("Value after variable declaration is " + value + "<br>");
+
+var value = 5;
+value++;
+document.write("Initial Value: 5" + "<br>");
+document.write("Value after increment is : " + value + "<br>");
+var sumValue = value + 7;
+document.write("Value after addition is : " + sumValue + "<br>");
+sumValue--;
+document.write("Value after decrement is : " + sumValue + "<br>");
+var remainder = sumValue % 3;
+document.write("The remainder is : " + remainder + "<br>");
+document.write("---------------------------------------------------------------------------<br>");
+
+// Question 4
+var movieTicket = 600;
+var buying = 5;
+var totalAmount = movieTicket * buying;
+document.write("Total cost to buy 5 tickets to a movie is " + totalAmount + "PKR" + "<br>");
+document.write("---------------------------------------------------------------------------<br>");
+
+// Question 5
+var tableNum = 10;
+var add = 1;
+add++;
+document.write("<h2>Table Of 100</h2>");
+document.write("10 * 1 = " + tableNum * --add + "<br>");
+document.write("10 * 2 = " + tableNum * ++add + "<br>");
+document.write("10 * 3 = " + tableNum * ++add + "<br>");
+document.write("10 * 4 = " + tableNum * ++add + "<br>");
+document.write("10 * 5 = " + tableNum * ++add + "<br>");
+document.write("10 * 6 = " + tableNum * ++add + "<br>");
+document.write("10 * 7 = " + tableNum * ++add + "<br>");
+document.write("10 * 8 = " + tableNum * ++add + "<br>");
+document.write("10 * 9 = " + tableNum * ++add + "<br>");
+document.write("10 * 10 = " + tableNum * ++add + "<br>");
+
