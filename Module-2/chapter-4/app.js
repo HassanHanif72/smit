6603afdc770442d9394d05b16b196a97e7105e5b
hassanhanif72 <hassanhanif72@gmail.com>
2024-08-17 01:36:43 +0500

    Upload Chapter 4

---
+++ Module-2/chapter-4/app.js
@@ -0,0 +1,19 @@
+// Declare 3 variables in one statement
+var firstName = "Hassan", lastName = "Hanif", email = "hassanhanif72@gmail.com";
+console.log("First Name: " + firstName , "\nLast Name: " + lastName ,  "\nEmail: " + email);
+
+// Declare 5 legal and 5 illegal variables names
+// Legal variables name
+var firstName
+var _firstName
+var firstName123
+var first_name
+var $firstname
+
+// illegal variables name
+
+// var 123firstName
+// var first name
+// var @firstName
+// var first-name
+// var first!name
