6afa855acd2beb14d9e3314fe2ecc6e26ac90855
hassanhanif72 <hassanhanif72@gmail.com>
2024-09-28 04:50:15 +0500

    Upload Chapter 14 to 17

---
+++ Module-2/chapter-14-17/app.js
@@ -0,0 +1,112 @@
+// Array
+// Question 1
+var arr = [];
+
+// Question 2
+var arr = {
+    names: []
+};
+
+// Question 3
+var arr = ['Hassan', 'Ahmed', 'Rameez'];
+
+// Question 4
+var arr = [2, 4, 6, 7, 9];
+
+// Question 5
+var arr = [true, false];
+
+// Question 6
+var arr = ['Mango', 3, true, null, { names: "Hassan" }, [1, 2, 3]];
+
+// Question 7
+// var arrQualification = ['SSC', 'HSC', 'BSC', 'BS', 'BCOM', 'MS', 'M', 'Phil', 'Phd'];
+// document.write("<h1>Qualifications</h1>");
+// document.write(arrQualification[0] + "<br>");
+// document.write(arrQualification[1] + "<br>");
+// document.write(arrQualification[2] + "<br>");
+// document.write(arrQualification[3] + "<br>");
+// document.write(arrQualification[4] + "<br>");
+// document.write(arrQualification[5] + "<br>");
+// document.write(arrQualification[6] + "<br>");
+// document.write(arrQualification[7] + "<br>");
+// document.write(arrQualification[8]);
+
+// Question 8
+// var studentArr = ['Hassan', 'Talha', 'Farman'];
+// var studentScore = [320, 230, 480];
+// var totalMarks = 500;
+// console.log("Score of Hassan is: " + studentScore[0] + "." + "Percentage " + (studentScore[0] / totalMarks) * 100 + "%");
+// console.log("Score of Talha is: " + studentScore[1] + "." + "Percentage " + (studentScore[1] / totalMarks) * 100 + "%");
+// console.log("Score of Farman is: " + studentScore[2] + "." + "Percentage " + (studentScore[2] / totalMarks) * 100 + "%");
+
+// Question 9
+// a
+// var colors = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange"];
+// var newArr = prompt('Add Updated Color: ');
+// colors.unshift(newArr);
+// document.write(colors);
+
+// b
+// var colors = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange"];
+// var newArr = prompt('Add Updated Color: ');
+// colors.push(newArr);
+// document.write(colors);
+
+// c
+// var colors = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange"];
+// var fisrtColor = prompt("Add First Color: ");
+// var secondColor = prompt("Add Second Color: ");
+// colors.unshift(fisrtColor, secondColor);
+// document.write(colors);
+
+// d
+// var colors = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange"];
+// colors.shift();
+// document.write(colors);
+
+// e
+// var colors = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange"];
+// colors.pop();
+// document.write(colors);
+
+// f
+// var colors = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange"];
+// var indexNum = +prompt("Add Color Index Number: ");
+// var colorName = prompt("Add Color Name: ");
+// if (indexNum >= 0 && indexNum <= colors.length) {
+//     colors.splice(indexNum, 0, colorName);
+// } else {
+//     alert("Invalid Index Number");
+// }
+// document.write(colors);
+
+// g
+// var colors = ["Red", "Green", "Blue", "Yellow", "Purple", "Orange"];
+// var index = +prompt("Delete Color Index: ");
+// var delNumColor = +prompt("Number of Delete color: ");
+// if (index >= 0 && index < colors.length && delNumColor > 0) {
+//     colors.splice(index, delNumColor);
+// } else {
+//     alert("Invalid index or number of colors!");
+// }
+// document.write(colors);
+
+// Question 10
+// var studentScores = [320, 230, 480, 120, 100];
+// studentScores.sort();
+// console.log(studentScores);
+
+// Question 11
+// var cityNames = ['Karachi', 'Lahore', 'Islamabad', 'Quetta', 'Peshawar'];
+// var selectedCities = [];
+// selectedCities = cityNames.slice(2,4);
+// console.log(selectedCities);
+
+// Question 12
+// var singleString = ["This", "is", "my", "cat"];
+// console.log("Array");
+// console.log(singleString);
+// console.log("String");
+// var newJoin = singleString.join(" ");
+// console.log(newJoin);
