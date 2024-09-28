f89b5cf4c3f71d7d2d1c03d18da9b7a8fdbb53a4
hassanhanif72 <hassanhanif72@gmail.com>
2024-09-28 05:39:37 +0500

    Upload 17 to 20

---
+++ Module-2/chapter-17-20/app.js
@@ -0,0 +1,31 @@
+// Question 1 Multi Dimensional Array
+var mulArr = [[], [], []];
+
+// Question 2
+// var mulArr = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
+// console.log(mulArr);
+
+// Question 3
+// for (i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// Question 4
+// var tableNum = +prompt("Enter Table Number: ");
+// var tableLenght = +prompt("Enter Table Length: ");
+// document.write(`<h1>Table Number: ${tableNum}</h1>`);
+// document.write(`<h1>Table Length: ${tableLenght}</h1>`);
+// for (i = 1; i <= tableLenght; i++) {
+//     document.write(tableNum + " * " + i + " = " + tableNum * i + "<br>");
+// }
+
+// Question 5
+// var fruits = ['Apple', 'Banana', 'Mango', 'Orange', 'Strawberry'];
+// for (i = 0; i < fruits.length; i++) {
+//     console.log("Element at index " + i + " is " + fruits[i]);
+// }
+
+// Question 6
+// var revCounting = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// var newCount = revCounting.reverse().join();
+// console.log(newCount);
